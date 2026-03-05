81608863b3e3b7b0d8e41be10769f07098c0c03a
thejan <thejan.hdk@gmail.com>
2026-03-05 15:47:53 +0530

    Created Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,14 @@
+class Assignment2 {
+public static int[] removeDuplicates(int[] arr){
+  // Implement logic here
+  return null;
+}
+
+  public static void main(String[] args) {
+      int[] arr = {1,2,2,3,4,4,5};
+      int[] result = removeDuplicates(arr);
+      for(int i=0; i<result.length; i++){
+              System.out.println(result[i]);
+      }
+  }
+}
